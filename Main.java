1c46ad7733203cca31c2c7f6e96aa48e181ec158
2382007 <shuimiracle@gmail.com>
2024-11-17 16:18:01 +0700

    Handson 8

--- Main.java
+++ Main.java
@@ -1,16 +1,19 @@
-import Entities.TodoList;
 import Repositories.TodoListRepository;
 import Repositories.TodoListRepositoryImpl;
 import Services.TodoListServicesImpl;
 import Services.TodoListServies;
 import Views.TodoListTerminalViews;
 import Views.TodoListViews;
+import config.Database;
 
 public class Main {
     public static void main(String[] args) {
-        TodoListRepository todoListRepository = new TodoListRepositoryImpl();
-        TodoListServies todoListService = new TodoListServicesImpl(todoListRepository);
-        TodoListViews todoListViews = new TodoListTerminalViews(todoListService);
-        todoListViews.run();
+        Database database = new Database("databaseku", "root", "", "Localhost", "3306");
+        database.setup();
+//        TodoListRepository todoListRepository = new TodoListRepositoryImpl();
+
+//        TodoListServies todoListService = new TodoListServicesImpl(todoListRepository);
+//        TodoListViews todoListViews = new TodoListTerminalViews(todoListService);
+//        todoListViews.run();
     }
 }
